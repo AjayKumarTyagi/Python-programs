780a61a414c01f72de0948272c7d9e25e0798ea9
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2019-10-01 08:23:12 +0530

    Xml url file added
    Gets url from xml file which is located online

---
+++ xmlurl.py
@@ -0,0 +1,8 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
